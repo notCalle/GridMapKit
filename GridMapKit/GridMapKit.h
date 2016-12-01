59f0e2124b53afc615357f60edfaeadad9150a1f
Calle Englund <calle@discord.bofh.se>
2016-12-01 23:08:01 +0100

    XCode project template
    Cocoa Touch Framework

---
+++ GridMapKit/GridMapKit.h
@@ -0,0 +1,19 @@
+//
+//  GridMapKit.h
+//  GridMapKit
+//
+//  Created by Calle Englund on 2016-12-01.
+//  Copyright © 2016 Calle Englund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GridMapKit.
+FOUNDATION_EXPORT double GridMapKitVersionNumber;
+
+//! Project version string for GridMapKit.
+FOUNDATION_EXPORT const unsigned char GridMapKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridMapKit/PublicHeader.h>
+
+
---
+++ GridMapKitTests/GridMapKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  GridMapKitTests.swift
+//  GridMapKitTests
+//
+//  Created by Calle Englund on 2016-12-01.
+//  Copyright © 2016 Calle Englund. All rights reserved.
+//
+
+import XCTest
+@testable import GridMapKit
+
+class GridMapKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
