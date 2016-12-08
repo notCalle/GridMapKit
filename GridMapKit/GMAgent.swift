99b3f6b1d17b2e7c5f2534ab5cb2ba0a682909bc
Calle Englund <calle@discord.bofh.se>
2016-12-08 07:12:36 -0500

    Add GMAgent class

---
+++ GridMapKit/GMAgent.swift
@@ -0,0 +1,13 @@
+//
+//  GMAgent.swift
+//  GridMapKit
+//
+//  Created by Calle Englund on 2016-12-08.
+//  Copyright © 2016 Calle Englund. All rights reserved.
+//
+
+import UIKit
+
+class GMAgent: NSObject {
+
+}
